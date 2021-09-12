8a0fcfa1eb345c757f4bb1286c870fc2eb1fc1b7
sheikasifahmed <sheikhasifahmed100@gmail.com>
2021-09-12 22:57:50 +0600

    image & hidden div  bug fixed

--- js/app.js
+++ js/app.js
@@ -6,11 +6,13 @@ const loadProducts = () => {
 };
 loadProducts();
 
-// show all product in UI 
+// show all product in UI
 const showProducts = (products) => {
+  console.log(products);
+  console.table(products);
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -74,7 +76,8 @@ const updateTaxAndCharge = () => {
 //grandTotal update function
 const updateTotal = () => {
   const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
+    getInputValue("price") +
+    getInputValue("delivery-charge") +
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
 };
